ecb4ee89952299996613439de2c25652c081390f
Manir Uddin <manir22sust@gmail.com>
2024-04-13 23:22:12 +0200

    Implement Responsive Single Page Design

--- src/routes/singlePage/SinglePage.jsx
+++ src/routes/singlePage/SinglePage.jsx
@@ -1,5 +1,35 @@
 import React from "react";
-
+import { Slider } from "../../components/slider/Slider";
+import { singlePostData, userData } from "../../lib/dummydata";
+import "./singlePage.scss";
 export const SinglePage = () => {
-  return <div>SinglePage</div>;
+  return (
+    <div className="singlePage">
+      <div className="details">
+        <div className="wrapper">
+          <Slider images={singlePostData.images} />
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1>{singlePostData.title} </h1>
+                <div className="address">
+                  <img src="/assets/pin.png" alt="" />
+                  <span>{singlePostData.address} </span>
+                </div>
+                <div className="price">${singlePostData.price}</div>
+              </div>
+              <div className="user">
+                <img src={userData.img} alt="" />
+                <span>{userData.name} </span>
+              </div>
+            </div>
+            <div className="bottom">{singlePostData.description}</div>
+          </div>
+        </div>
+      </div>
+      <div className="features">
+        <div className="wrapper"></div>
+      </div>
+    </div>
+  );
 };
