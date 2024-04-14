b472a084f4ef2595030ec6e8e738ff5320407ec1
Manir Uddin <manir22sust@gmail.com>
2024-04-14 18:58:08 +0200

    update Image Slide function

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -4,21 +4,34 @@ import "./slider.scss";
 export const Slider = ({ images }) => {
   const [imageIndex, setImageIndex] = useState(null);
 
+  // const changeSlide = (direction) => {
+  //   if (direction === "left") {
+  //     if (imageIndex === 0) {
+  //       setImageIndex(images.length - 1);
+  //     } else {
+  //       setImageIndex(imageIndex - 1);
+  //     }
+  //   } else {
+  //     if (imageIndex === images.length - 1) {
+  //       setImageIndex(0);
+  //     } else {
+  //       setImageIndex(imageIndex + 1);
+  //     }
+  //   }
+  // };
+
   const changeSlide = (direction) => {
     if (direction === "left") {
-      if (imageIndex === 0) {
-        setImageIndex(images.length - 1);
-      } else {
-        setImageIndex(imageIndex - 1);
-      }
+      setImageIndex((prevIndex) =>
+        prevIndex === 0 ? images.length - 1 : prevIndex - 1
+      );
     } else {
-      if (imageIndex === images.length - 1) {
-        setImageIndex(0);
-      } else {
-        setImageIndex(imageIndex + 1);
-      }
+      setImageIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
     }
   };
+
   return (
     <div className="slider">
       {imageIndex !== null && (
